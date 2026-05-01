991aec639670eea4b648c215b9e70394aa9740ac
Milan Koyš <milankoys@tuta.com>
2026-05-01 11:44:49 +0200

    feat: response fix

--- src/app/views/register/register.ts
+++ src/app/views/register/register.ts
@@ -17,6 +17,7 @@ import { Nullable } from '@contracts';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { Error } from 'app/contracts/error';
 
 interface RegisterForm {
   firstName: FormControl<Nullable<string>>;
@@ -137,12 +138,15 @@ export class Register {
 
     this._httpClient.post('https://autoskolaimrich.sk/api/register', values).subscribe({
       next: () => this._router.navigateByUrl('/register-success'),
-      error: () =>
+      error: (response: Error) => {
         this._messageService.add({
           summary: 'Error',
-          detail: 'Niečo sa zlé sa stalo, pokúste sa znova o pár minút.',
-          severity: 'danger',
-        }),
+          detail: response.error?.message
+            ? response.error.message
+            : 'Niečo sa zlé sa stalo, pokúste sa znova o pár minút.',
+          severity: 'warn',
+        });
+      },
     });
   }
 
